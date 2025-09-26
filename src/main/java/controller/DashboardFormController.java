e450c4ad62ea65cbaffa23e754879eda294b2125
nethmakannangara <nethmakannangara007@gmail.com>
2025-09-26 23:06:18 +0530

    add button action in DashboardForm controller

--- src/main/java/controller/DashboardFormController.java
+++ src/main/java/controller/DashboardFormController.java
@@ -1,4 +1,55 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+
+    Stage itemStage = new Stage();
+
+    @FXML
+    private JFXButton btnCustomerManagement;
+
+    @FXML
+    private JFXButton btnItemManagement;
+
+    @FXML
+    private JFXButton btnOrderDetailsManagament;
+
+    @FXML
+    private JFXButton btnOrderManagement;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOrderDetailsOnAction(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+        try {
+            itemStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/item.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        itemStage.setResizable(false);
+        itemStage.show();
+    }
+
+    @FXML
+    void btnOrderOnAction(ActionEvent event) {
+
+    }
+
 }
